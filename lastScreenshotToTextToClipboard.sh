9ceabae0a8e094401b339335c3acfebfd6d7e5b2
Tomas Halgas <tom.halgas@gmail.com>
2021-02-22 22:37:26 +0000

    Updated screenshot to text to clipboard script to support latest versions of Mac OS

--- lastScreenshotToTextToClipboard.sh
+++ lastScreenshotToTextToClipboard.sh
@@ -6,12 +6,20 @@
 # would return something like: /Users/tomash/Desktop/Screen
 # and echo $event
 # Shot 2020-02-10 at 21.50.11.png 592
+# also note that since some version of Mac OS it's "Screenshot", not "Screen Shot"
 fswatch --batch-marker=EOF -xn ~/Desktop/ | while read file event; do 
 	# to enable echoing all file activity
 	# echo $file $event
 	if [[ "$file $event" == */Screen\ Shot\ 20*.png\ 592 ]]; then
 		# find last screenshot, extract text, change newlines for spaces, copy it to clipboard
 		tesseract ~/Desktop/"$(ls -1 ~/Desktop/ | grep Screen\ Shot\ 20 | tail -1)" .lastScreenshotToTextToClipboard.temp && tr '\n' ' ' < .lastScreenshotToTextToClipboard.temp.txt | pbcopy
+		# delete temporary file
+		rm .lastScreenshotToTextToClipboard.temp.txt
+	fi
+
+	if [[ "$file $event" == */Screenshot\ 20*.png\ 592 ]]; then
+		# find last screenshot, extract text, change newlines for spaces, copy it to clipboard
+		tesseract ~/Desktop/"$(ls -1 ~/Desktop/ | grep Screenshot\ 20 | tail -1)" .lastScreenshotToTextToClipboard.temp && tr '\n' ' ' < .lastScreenshotToTextToClipboard.temp.txt | pbcopy
 		# delete temporary file
 		rm .lastScreenshotToTextToClipboard.temp.txt
 	fi
